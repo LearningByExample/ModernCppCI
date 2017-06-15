6e21a2084258865d65c11afa9a34a9f2a9d34209
Juan Medina <jamedina@gmail.com>
2017-06-15 23:38:25 +0100

    updated

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include "include/Calc.hpp"
+#include "Calc.hpp"
 
 int main(int argc, char *argv[]){
    std::cout << "hello world";
---
+++ main/src/calc.cpp
@@ -0,0 +1,1 @@
+// this should have a class implementation
