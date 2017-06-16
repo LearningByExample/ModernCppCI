44b7f3e3958c575c4cbe61b85cbb366b5c1cac69
Juan Medina <jamedina@gmail.com>
2017-06-16 21:52:37 +0100

    more refator

--- test/src/CalcTest.cpp
+++ test/src/CalcTest.cpp
@@ -3,15 +3,23 @@
 
 namespace ModernCppCI {
 
-	namespace Test {
-		TEST_CASE("calc should be created", "[calc]") {
-			Calc* calc = new Calc();
+    namespace Test {
+        TEST_CASE("calc should be created", "[calc]") {
+            Calc *calc = new Calc();
 
-			REQUIRE(calc != nullptr);
+            REQUIRE(calc != nullptr);
 
-			delete calc;
-		}
-	}
+            delete calc;
+        }
+
+        TEST_CASE("calc should be created2", "[calc]") {
+            Calc *calc = new Calc();
+
+            REQUIRE(calc != nullptr);
+
+            delete calc;
+        }
+    }
 
 }
 
